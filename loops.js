c20bd6d16cf2d5ee154085761ff484b69ce415f2
sagarksp <kspsagar01@gmail.com>
2025-06-07 13:09:09 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,7 @@
+// FOR LOOPS
+
+//loops has three main parameters initialization and condition and increment and decrement 
+//loops is continously executed till the condition is satisfied
+for(let i=0;i<4;i++){
+    console.log("hello world")
+}
