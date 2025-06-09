54c1bcf7b179434de1e99ff227cfe7d5864d0fa7
sagarksp <kspsagar@gmail.com>
2025-06-09 17:14:33 +0530

    array1

---
+++ Array_level0/array1.js
@@ -0,0 +1,10 @@
+let arr = [10,6,2,0,8,3,223]
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        console.log("even number " + arr[i])
+    }
+    else{
+        console.log("odd number " + arr[i])
+    }
+}
