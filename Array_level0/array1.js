361d6c0c171364be5ff186f15a0d04e4c825db6f
sagarksp <kspsagar01@gmail.com>
2025-06-10 17:02:41 +0530

    finding index of a number in an given array

--- Array_level0/array1.js
+++ Array_level0/array1.js
@@ -1,11 +1,1 @@
-let arr = [10,6,2,0,8,3,223]
-
-for(let i=0;i<arr.length;i++){
-    if(arr[i]%2==0){
-        console.log("even number " + arr[i])
-    }
-    else{
-        console.log("odd number " + arr[i])
-    }
-}
-//working with the array and function and loops
+//write a function that searches 
---
+++ loops_array/one.js
@@ -0,0 +1,14 @@
+//write a function that searches for an element in an array and returns the index, if the element is not present then just return -1
+
+let arr = [4,2,0,10,8,30]
+
+function searchElement(arr,x){
+    for(let i=0; i<arr.length;i++){
+        if(arr[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}
+let result = searchElement(arr,8)
+console.log("index = "+ result)
