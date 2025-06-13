8aa23c843a91e388f2dd30d0b1a22b16be364d7f
sagarksp <kspsagar@gmail.com>
2025-06-13 16:22:07 +0530

    patterns

---
+++ patterns/pattern5.js
@@ -0,0 +1,18 @@
+let n = 5 ; 
+
+// for(let i = 1 ; i<=n;i++){
+//     let row  = "";
+//     for (let j = 1;j<=n-i+1; j++){
+        
+//         row = row + " " + j
+//     }
+//     console.log(row)
+// }
+
+for(let i=0; i<n;i++){
+   let row = ""
+    for(let j=0;j<n-i;j++){
+        row = row + " " + (j+1)
+    }
+    console.log(row)
+}
---
+++ patterns/starpattern.js
@@ -0,0 +1,14 @@
+//star pattern
+
+let n = 5 ;
+
+for(let i=0;i<n;i++){
+    let row = "";
+    for(let j=0;j<n;j++){
+        row = row + " * "
+    }
+    console.log(row)
+}
+
+//pyramid
+
