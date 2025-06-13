7a1834d1708fd50e65b6f6449f0ac677445398b8
sagarksp <kspsagar01@gmail.com>
2025-06-13 16:22:07 +0530

    patterns

--- Array_level0/array1.js
+++ Array_level0/array1.js
@@ -1,1 +1,5 @@
 //write a function that searches 
+
+let a=6;
+let b=7
+console.log(a+b)
---
+++ patterns/pattern2.js
@@ -0,0 +1,10 @@
+let n = 10 ; 
+
+for(let i=0; i <n ; i++){
+    let row = "";
+     for(let j = 0 ;j<=i;j++){
+    // for(let j = 0 ;j<i+1;j++){
+        row = row + " * ";
+    }
+    console.log(row)
+}
