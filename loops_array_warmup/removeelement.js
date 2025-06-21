5155847c32bc36901211d46b6a7aa34ea1466ffc
sagarksp <kspsagar@gmail.com>
2025-06-21 19:03:29 +0530

    remove element

---
+++ loops_array_warmup/removeelement.js
@@ -0,0 +1,20 @@
+function remove(nums,val){
+
+    let count=0;
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==val){
+            nums[i] = nums[i+1]
+        }
+        else  count++
+        
+       
+    }
+    return count;
+
+}
+
+let nums = [0,1,2,2,3,0,4,0,2]
+let val  = 3
+
+console.log(remove(nums,val))
