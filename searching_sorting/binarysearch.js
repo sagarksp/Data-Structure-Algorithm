3423d4c6331c5c52930d2a088fcc61a4252ecf2e
sagarksp <kspsagar01@gmail.com>
2025-07-07 17:39:05 +0530

    binary search

---
+++ searching_sorting/binarysearch.js
@@ -0,0 +1,32 @@
+var search = function(nums, target) {
+    let left = 0;
+    let right = (nums.length-1);
+
+    while(right>=left){
+        let middle = Math.floor((left+right)/2);
+
+        if(target === nums[middle]){
+            
+            return middle
+        }
+        
+        else if(nums[middle]<target){
+            left = middle+1
+             
+             
+        }
+        else{
+            right = middle-1
+             
+             
+        }
+     
+       
+    }
+    
+    return -1;
+    
+};
+
+let nums = [-1,0,3,5,9,12]
+console.log(search(nums, 9))
